67f61dd4d9b802a390b7f91e04bf14aa76e99f1a
nur-alam <nuralam862@gmail.com>
2019-05-05 09:22:20 +0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+jQuery(document).ready(function($) {
+
+	var slider = $('.slides');
+
+	var width = $('.team-single').width(),
+		len =  $('.team-single').length,
+		totalSliderWidth = width*len,
+		animationSpeed = 1000,
+		pause = 3000;
+
+	slider.css("width",totalSliderWidth+(len*15));
+
+	var slideCount = 0,
+		slidableCount = len - 3,
+		RightToLeftMoveInterval, LeftToRightMoveInterval;
+
+	function RightToLeftMoveSlider(){
+		RightToLeftMoveInterval = setInterval(()=>{
+			if(slidableCount == slideCount){
+				clearInterval(RightToLeftMoveInterval);
+				LeftToRightMoveSlide();
+			}else{
+				$('.slides').animate({
+					'margin-left' : '-='+(width+15)
+				},animationSpeed);
+				slideCount++;
+			}
+		},pause);		
+	}
+
+	function LeftToRightMoveSlide(){
+		LeftToRightMoveInterval = setInterval(() => {
+			$('.slides').animate({
+				'margin-left' : '+='+(width+15)
+			},animationSpeed);
+			slideCount--;
+			if(slideCount == 0){
+				clearInterval(LeftToRightMoveInterval);
+				RightToLeftMoveSlider();
+			}
+		}, pause);
+	}
+
+	RightToLeftMoveSlider();	
+
+});
